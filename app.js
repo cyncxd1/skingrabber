d5d09fba52471b2465c51b11033c6d90ee69cbf7
cyncxd <79019625+cyncxd1@users.noreply.github.com>
2024-08-30 22:03:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ search.onkeydown = (inp) => {
 	let search = document.getElementById('search'),
 		img = document.querySelector('.card-img-top'),
 		src = `https://minotar.net/skin/${search.value}`
+
 	if (inp.key === 'Enter') {
-		if (search.value === '') {
-			img.src = 'https://file.garden/ZqPrCJ2v7HmQTmrR/transparent.png'
-			img.style.position = 'absolute'
-            img.style.pointerEvents = 'none'
-            img.style.opacity = 0
-		} else {
-			img.src = src
-			img.style.position = 'static'
-            img.style.pointerEvents = 'all'
-            img.style.opacity = 1
+		switch (search.value) {
+			case '':
+				img.src = 'https://file.garden/ZqPrCJ2v7HmQTmrR/transparent.png'
+				img.style.position = 'absolute'
+				img.style.opacity = 0
+				break
+			default:
+				img.src = src
+				img.style.position = 'static'
+				img.style.opacity = 1
 		}
 	}
 }
 
 document.body.onload = () => {
-
 	search.value = ''
 }
